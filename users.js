c9ccb599ad4dbcdca8c48747b3feeee18ace1817
rafgt88 <91056534+rafgt88@users.noreply.github.com>
2025-11-13 05:54:15 +0300

    Add multiple users to users.js

--- users.js
+++ users.js
@@ -1,6 +1,14 @@
 const users = [
     {
-        username: "barakat",
+        username: "Barakat",
+        passwordHash: "4f9f10b304cfe9b2b11fcb1387f694e18f08ea358c7e9f567434d3ad6cbd7fc4"
+    }
+    ,{
+        username: "Numan",
+        passwordHash: "4f9f10b304cfe9b2b11fcb1387f694e18f08ea358c7e9f567434d3ad6cbd7fc4"
+    }
+    ,{
+        username: "Ziad",
         passwordHash: "4f9f10b304cfe9b2b11fcb1387f694e18f08ea358c7e9f567434d3ad6cbd7fc4"
     }
 ];
